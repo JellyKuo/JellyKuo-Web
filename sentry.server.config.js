e0ad72130ad5769707d71eec26902246a74699c1
JellyKuo <jellykuo1234@gmail.com>
2025-11-26 22:17:56 +0800

    Fix extra import

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -1,15 +1,10 @@
 import * as Sentry from "@sentry/astro";
-import { nodeProfilingIntegration } from "@sentry/profiling-node";
 import config from "./src/config/config.json";
 Sentry.init({
   dsn: config.sentry.dsn,
   // Adds request headers and IP for users, for more info visit: for more info visit:
   // https://docs.sentry.io/platforms/javascript/guides/astro/configuration/options/#sendDefaultPii
   sendDefaultPii: true,
-  integrations: [
-    // Add our Profiling integration
-    nodeProfilingIntegration(),
-  ],
   // Enable logs to be sent to Sentry
   enableLogs: true,
   // Define how likely traces are sampled. Adjust this value in production,
